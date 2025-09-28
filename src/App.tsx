e44381b48b0535da620466f8f4397365119d1acb
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2025-09-28 21:31:03 +0600

    free heating control

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import FormHome from "./pages/eligibility-form";
+import FormHome from "./pages/eligibility-form/FormHome";
 
 const queryClient = new QueryClient();
 
